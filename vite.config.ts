032e92d42b2d1e9a77481eb06821381f3f43bb21
Федор Кузнецов <fedorkuznecov@MacBook-Pro-Fedor.local>
2026-02-06 18:05:56 +0300

    Add repo-name as a base

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: 'risk-fluent-v2',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
